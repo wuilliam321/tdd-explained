876a3b467247341e3af963f1aac3338d6236a0fc
Wuilliam Lacruz <wuilliam.lacruz@pedidosya.com>
2020-04-27 20:50:28 -0300

    Adding only the needed code to make it pass, no more

--- src/romans.test.js
+++ src/romans.test.js
@@ -1,9 +1,11 @@
+import { convert } from "./romans";
+
 describe('convert arabic to romans', () => {
     it('convert 1 to I', () => {
         const arabic = 1;
 
         const result = convert(arabic);
-        
+
         expect(result).toBe('I');
     })
 })
