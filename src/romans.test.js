a700d381bb04a5e320a58adf602c839e2398f707
Wuilliam Lacruz <wuilliam.lacruz@pedidosya.com>
2020-04-27 20:48:04 -0300

    First failing code, only sufficient code to make prod code fail

---
+++ src/romans.test.js
@@ -0,0 +1,9 @@
+describe('convert arabic to romans', () => {
+    it('convert 1 to I', () => {
+        const arabic = 1;
+
+        const result = convert(arabic);
+        
+        expect(result).toBe('I');
+    })
+})
