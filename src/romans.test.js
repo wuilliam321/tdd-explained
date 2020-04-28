aeeb4cf9a885c0a7ffad6472b3e1c6540efc9d0a
Wuilliam Lacruz <wuilliam.lacruz@pedidosya.com>
2020-04-27 23:29:04 -0300

    Add more test cases

--- src/romans.test.js
+++ src/romans.test.js
@@ -1,6 +1,8 @@
 import { convert } from './romans';
 
 const conversions = [
+    [3, 'III'],
+    [2, 'II'],
     [1, 'I']
 ];
 describe('convert arabic to romans', () => {
@@ -12,28 +14,5 @@ describe('convert arabic to romans', () => {
 
             expect(result).toBe(roman);
         });
-    });
-    it('convert 1 to I', () => {
-        const arabic = 1;
-
-        const result = convert(arabic);
-
-        expect(result).toBe('I');
-    });
-
-    it('convert 2 to II', () => {
-        const arabic = 2;
-
-        const result = convert(arabic);
-
-        expect(result).toBe('II');
-    });
-
-    it('convert 3 to III', () => {
-        const arabic = 3;
-
-        const result = convert(arabic);
-
-        expect(result).toBe('III');
     });
 });
